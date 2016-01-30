36f6ee3dabef633208f5b99f861b0a2c4f3a5a26
Fernando de Morais <mnando@gmail.com>
2016-01-30 02:58:41 -0200

    Changing how PushToHeroku task interacts with git repository.

--- Tasks/PushToHeroku/PushToHeroku.ps1
+++ Tasks/PushToHeroku/PushToHeroku.ps1
@@ -37,14 +37,14 @@ function Invoke-GitCommand
 
     $output = "$GitCommand"
     foreach($line in $result) {
-        $val = ""
+        $val = "[git] "
         if($line.GetType().Name -eq "ErrorRecord") {
             if($line -match ".*error:.*" -or $line -match ".*fatal:.*"){
-                throw ($line)
+                throw ("$output`r`n[git] $line")
             }
-            $val = $line.Exception.Message
+            $val += $line.Exception.Message
         }else{
-            $val = $line
+            $val += $line
         }
         
         $output = "$output`r`n$val"
@@ -70,6 +70,9 @@ if(-not($CommitMessage)){
 $RequestedFor = $Env:RELEASE_REQUESTEDFOR;
 $RequestedForEmail = "vsts@$AppName.git.heroku.com";
 
+$ReleaseDirectory = $Env:AGENT_RELEASEDIRECTORY;
+$GitDirectory = Join-Path -Path $ReleaseDirectory -ChildPath 'heroku'
+
 $HerokuUrl = "https://vsts:$ApiKey@git.heroku.com:443/$AppName.git"
 
 Write-Verbose "Entering script PushToHeroku.ps1" 
@@ -85,47 +88,81 @@ Write-Verbose "Entering script PushToHeroku.ps1"
     Write-Verbose "RequestedFor = $RequestedFor"
     Write-Verbose "RequestedForEmail = $RequestedForEmail";
     Write-Verbose "HerokuUrl = $HerokuUrl"
+    Write-Verbose "ReleaseDirectory = $ReleaseDirectory"
+    Write-Verbose "GitDirectory = $GitDirectory"
 
-Write-Host "Setting working directory to '$PushRoot'"
-    Set-Location $PushRoot
 
+Write-Host "Setting working directory to '$GitDirectory'"
+    New-Item -Path $GitDirectory -ItemType Directory
+    Set-Location $GitDirectory
+
+
+Write-Host "Cloning heroku repository"
+    Invoke-GitCommand -GitCommand "git clone $HerokuUrl"
+    Write-Verbose "Files cloned: "
+    Get-ChildItem -Recurse -Force | %{ Write-Verbose $_.FullName }
+    $GitDirectory = Join-Path -Path $GitDirectory -ChildPath $AppName    
+Write-Host "Heroku cloned"
+
+
+Write-Host "Setting working directory to '$GitDirectory'"
+    Set-Location $GitDirectory
+
+
+Write-Host "Cleaning up files"
+    Get-ChildItem -Recurse -Force  | `
+        ?{ $_.FullName -notlike "*.git*" } | `
+            Remove-Item -Force
+            
+    Write-Verbose "Files after cleaning up: "
+    Get-ChildItem -Recurse -Force | %{ Write-Verbose $_.FullName }
+Write-Host "Files cleaned"
+    
+
+Write-Host "Copying files from '$PushRoot'"
+    Write-Verbose "Files to copy from '$PushRoot'"
+    Get-ChildItem -Path $PushRoot -Recurse -Force | %{ Write-Verbose $_.FullName }
+
+    $copyPath = Join-Path -Path $PushRoot -ChildPath '*'
+    Write-Verbose "copyPath = $copyPath"
+    
+    Copy-Item $copyPath -Recurse -Force
+    
+    Write-Verbose "Files after copy: "
+    Get-ChildItem -Recurse -Force | %{ Write-Verbose $_.FullName }
+Write-Host "Files copied"
+    
 
 Write-Host "Writing .gitignore file"
     $GitIgnorePath = Join-Path -Path $PushRoot -ChildPath '.gitignore'
     $GitIgnore | Set-Content $GitIgnorePath
+Write-Host ".gitignore written"
 
 
-Write-Host "Initializing git"
-    Invoke-GitCommand -GitCommand "git init"
+Write-Host "Initializing git config"
     Invoke-GitCommand -GitCommand "git config --local user.name `"$RequestedFor`""
     Invoke-GitCommand -GitCommand "git config --local user.email `"$RequestedForEmail`""
-
-Write-Host "Adding Heroku remote"
-    Invoke-GitCommand -GitCommand "git remote add heroku $HerokuUrl"
-    Invoke-GitCommand -GitCommand "git remote -v" -OnlyDebug $true
+Write-Host "Git config initialized"
 
 
 Write-Host "Adding files"
     Invoke-GitCommand -GitCommand "git add ."
+Write-Host "Files added"
 
 
 Write-Host "Committing changes"
     Invoke-GitCommand -GitCommand "git commit -m `"$CommitMessage`""
     Invoke-GitCommand -GitCommand "git status" -OnlyDebug $true
-    
-    
-if($ForceOnPushing -eq "true"){
-    Write-Host "Starting to push changes"
-    Invoke-GitCommand -GitCommand "git push -f heroku master"    
-} else {
-    Write-Host "Starting to pull"
-    Invoke-GitCommand -GitCommand "git pull heroku master"
-     
-    Write-Host "Starting to push changes"
-    Invoke-GitCommand -GitCommand "git push heroku master"
-}
-    
-    Write-Host "Push finished."
+Write-Host "Changes committed"
+
+
+Write-Host "Starting to push changes"
+    if($ForceOnPushing -eq "true"){
+        Invoke-GitCommand -GitCommand "git push -f origin master"
+    } else {
+        Invoke-GitCommand -GitCommand "git push origin master"
+    }
+Write-Host "Push finished."
 
 
 Write-Verbose "Leaving script PushToHeroku.ps1"
